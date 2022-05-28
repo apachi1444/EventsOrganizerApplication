0fe51f2b9f0e63eb6f1063864c776d2b317ea0a2
asmae01 <asmaeoubraim331999@gmil.com>
2022-05-28 02:22:11 +0000

    Add budget page and modification in mychickList and switchMainPage and settingPage

--- lib/screens/guestPages/guestSwitchMainPage.dart
+++ lib/screens/guestPages/guestSwitchMainPage.dart
@@ -1,10 +1,13 @@
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:pfs/screens/guestPages/events/budget/BudgetPage.dart';
+import 'package:pfs/screens/guestPages/events/chickList/MyChickList.dart';
 import 'package:pfs/screens/guestPages/home/home_page.dart';
 import 'package:pfs/screens/guestPages/profilePage/profilePageGuest.dart';
 
 import '../../extensions/constant_colors.dart';
+import '../userPages/parametersPages/settings/settings_page.dart';
 
 class GuestSwitchMainPage extends StatefulWidget {
   const GuestSwitchMainPage({Key? key}) : super(key: key);
@@ -17,9 +20,14 @@ class _GuestSwitchMainPageState extends State<GuestSwitchMainPage> {
   int _index = 1;
   final items = <Widget>[
     const Icon(Icons.home),
-    const Icon(Icons.home),
+    const Icon(Icons.article_outlined),
+    const Icon(Icons.search),
+    const Icon(Icons.account_balance),
+    const Icon(Icons.person),
+
+
   ];
-  final screens = [const HomePageGuest(), const ProfilePageGuest()];
+  final screens = [const HomePageGuest(),const MyCheckingList(),const BudgetPage() ];
   final navigationKey = GlobalKey<CurvedNavigationBarState>();
   @override
   Widget build(BuildContext context) {
@@ -29,7 +37,7 @@ class _GuestSwitchMainPageState extends State<GuestSwitchMainPage> {
             .copyWith(iconTheme: const IconThemeData(color: Colors.green)),
         child: CurvedNavigationBar(
           key: navigationKey,
-          backgroundColor: Colors.blue,
+          backgroundColor: Colors.green,
           color: Colors.red,
           buttonBackgroundColor: const Color(ConstantColors.KGreyColor),
           items: items,
