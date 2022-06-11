0415bcf0d7d9a55e6c1eca521202d1366a1e491c
unknown <yessinejawa@gmail.com>
2022-06-11 19:53:35 +0200

    adding the first name and the last name of the user when logged in the professional part

--- lib/screens/professionalPages/home/homePage.dart
+++ lib/screens/professionalPages/home/homePage.dart
@@ -2,19 +2,22 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../../../services/authService.dart';
+import '../../../sharedPreferences/ProfessionalPreferences.dart';
 
 class HomePageProfessional extends StatelessWidget {
   const HomePageProfessional({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final String? firstName = ProfessionalPreferences.getFirstName();
+    final String? lastName = ProfessionalPreferences.getLastName();
     return SafeArea(
       child: Scaffold(
         body: Center(
           child : Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Text('Welcome Home User '),
+               Text('Welcome Home $firstName $lastName'),
               ElevatedButton(
                   onPressed:(){
                     AuthService().signOut();
