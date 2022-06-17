ca19f9309833ac49d91fb12e1b44a0c038ed02bb
unknown <yessinejawa@gmail.com>
2022-06-17 22:54:09 +0200

    fixing the updating professional's profile

--- lib/screens/professionalPages/parametersPages/editProfile/edit_profile_page.dart
+++ lib/screens/professionalPages/parametersPages/editProfile/edit_profile_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:pfs/screens/professionalPages/parametersPages/editProfile/editProfileParts/buttonUpdate.dart';
 import 'package:pfs/screens/professionalPages/parametersPages/editProfile/edit_profile_screen.dart';
+import 'package:pfs/services/settingsServices.dart';
 import 'package:pfs/sharedPreferences/ProfessionalPreferences.dart';
 
 import '../../../../extensions/constants.dart';
@@ -30,108 +31,113 @@ class EditProfileProfessionalPage extends StatelessWidget {
     //     ]),
     //   )),
     // );
+    final _uid = AuthService().getCurrentIdUser();
     return Scaffold(
-      body: ListView(
-        physics: const BouncingScrollPhysics(
-          parent: AlwaysScrollableScrollPhysics(),
-        ),
-        children: [
-          Container(
-              height: MediaQuery.of(context).size.height * 0.2,
-              decoration: const BoxDecoration(
-                  color: Color(ConstantColors.KPinkColor),
-                  image: DecorationImage(
-                      fit: BoxFit.cover,
-                      image: AssetImage('assets/avatar.jpg'))),
-              child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 15, vertical: 15),
-                      child: PopupMenuButton(
-                          icon: const Icon(Icons.arrow_circle_left_rounded,
-                              color: Colors.white, size: 30),
-                          itemBuilder: (_) {
-                            return <PopupMenuItem<String>>[
-                              const PopupMenuItem(
-                                  child: Text('GoBack'), value: 'go back')
-                            ];
-                          },
-                          onSelected: (index) {
-                            Navigator.pop(context);
-                          }),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 15, vertical: 15),
-                      child: PopupMenuButton(
-                          icon: const Icon(Icons.logout,
-                              color: Colors.white, size: 30),
-                          itemBuilder: (_) {
-                            return <PopupMenuItem<String>>[
-                              const PopupMenuItem(
-                                  child: Text('Logout'), value: 'logout')
-                            ];
-                          },
-                          onSelected: (index) {
-                            AuthService().signOut();
-                            Navigator.pushNamedAndRemoveUntil(
-                                context, '/authProfessional', (r) => false);
-                          }),
-                    )
-                  ])),
-          Container(
-              transform: Matrix4.translationValues(0.0, -40.0, 0.0),
-              padding: const EdgeInsets.symmetric(horizontal: 15),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.center,
+      body: StreamBuilder(
+        stream: SettingsService(professionalUid: _uid).readOneProfessional(),
+        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+          return ListView(
+            physics: const BouncingScrollPhysics(
+              parent: AlwaysScrollableScrollPhysics(),
+            ),
+            children: [
+              Container(
+                  height: MediaQuery.of(context).size.height * 0.2,
+                  decoration: const BoxDecoration(
+                      color: Color(ConstantColors.KPinkColor),
+                      image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage('assets/avatar.jpg'))),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 15, vertical: 15),
+                          child: PopupMenuButton(
+                              icon: const Icon(Icons.arrow_circle_left_rounded,
+                                  color: Colors.white, size: 30),
+                              itemBuilder: (_) {
+                                return <PopupMenuItem<String>>[
+                                  const PopupMenuItem(
+                                      child: Text('GoBack'), value: 'go back')
+                                ];
+                              },
+                              onSelected: (index) {
+                                Navigator.pop(context);
+                              }),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 15, vertical: 15),
+                          child: PopupMenuButton(
+                              icon: const Icon(Icons.logout,
+                                  color: Colors.white, size: 30),
+                              itemBuilder: (_) {
+                                return <PopupMenuItem<String>>[
+                                  const PopupMenuItem(
+                                      child: Text('Logout'), value: 'logout')
+                                ];
+                              },
+                              onSelected: (index) {
+                                AuthService().signOut();
+                                Navigator.pushNamedAndRemoveUntil(
+                                    context, '/authProfessional', (r) => false);
+                              }),
+                        )
+                      ])),
+              Container(
+                  transform: Matrix4.translationValues(0.0, -40.0, 0.0),
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
                     children: [
-                      const CircleAvatar(
-                        radius: 45,
-                        backgroundImage:
-                            AssetImage('assets/avatar.jpg'),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          const CircleAvatar(
+                            radius: 45,
+                            backgroundImage: AssetImage('assets/avatar.jpg'),
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.push(context, MaterialPageRoute(
+                                  builder: (BuildContext context) {
+                                return const EditProfileScreen(key: null);
+                              }));
+                            },
+                            child: Container(
+                                width: 100,
+                                height: 35,
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 10),
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(20),
+                                    color: Colors.white,
+                                    border: Border.all(
+                                      color: const Color(
+                                          ConstantColors.KPinkColor),
+                                    )),
+                                child: const Center(
+                                    child: Text('Edit',
+                                        style: TextStyle(
+                                          fontSize: 17,
+                                          color:
+                                              Color(ConstantColors.KPinkColor),
+                                          fontWeight: FontWeight.bold,
+                                        )))),
+                          )
+                        ],
                       ),
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.push(context, MaterialPageRoute(
-                              builder: (BuildContext context) {
-                            return const EditProfileScreen(key: null);
-                          }));
-                        },
-                        child: Container(
-                            width: 100,
-                            height: 35,
-                            padding:
-                                const EdgeInsets.symmetric(horizontal: 10),
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(20),
-                                color: Colors.white,
-                                border: Border.all(
-                                  color: const Color(
-                                      ConstantColors.KPinkColor),
-                                )),
-                            child: const Center(
-                                child: Text('Edit',
-                                    style: TextStyle(
-                                      fontSize: 17,
-                                      color:
-                                          Color(ConstantColors.KPinkColor),
-                                      fontWeight: FontWeight.bold,
-                                    )))),
-                      )
+                      const SizedBox(height: 10),
                     ],
-                  ),
-                  const SizedBox(height: 10),
-                ],
-              )),
-          AllRowsOfFieldsProfessionalData()
-        ],
+                  )),
+              AllRowsOfFieldsProfessionalData()
+            ],
+          );
+        },
       ),
     );
   }
